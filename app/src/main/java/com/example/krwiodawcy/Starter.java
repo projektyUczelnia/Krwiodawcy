b475f20f9922a70a56b657c44c701cc7ed88614f
jas <janekpro996@gmail.com>
2019-05-17 15:32:24 +0200

    1commit

---
+++ app/src/main/java/com/example/krwiodawcy/Starter.java
@@ -0,0 +1,23 @@
+package com.example.krwiodawcy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Starter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler= new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Starter.this, LogIn.class);
+                startActivity(intent);
+            }
+        },3000);
+    }
+}
